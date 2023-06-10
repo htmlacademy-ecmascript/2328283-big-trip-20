db03802d8e9d8b42fd808ec70618b318cd47a16b
Alexander <nar.080@mail.ru>
2023-06-10 12:42:02 +0300

    4.17. Шаблонизируй то

--- src/main.js
+++ src/main.js
@@ -1,14 +1,21 @@
 import MainPresenter from './presenter/main-presenter.js';
 import WaypointPresenter from './presenter/waypoint-presenter.js';
 import WaypointModel from './model/waypoint-model.js';
-
 const tripMainElement = document.querySelector('.trip-main');
-const tripControlsFiltersElement = document.querySelector('.trip-controls__filters');
+const tripControlsFiltersElement = document.querySelector(
+  '.trip-controls__filters'
+);
 const tripEventsSection = document.querySelector('.trip-events');
-
 const waypointModel = new WaypointModel();
-const mainPresenter = new MainPresenter({tripMain: tripMainElement, tripControlsFiltres: tripControlsFiltersElement, tripEventsSection: tripEventsSection});
-const waypointPresenter = new WaypointPresenter({waypointContainer: tripEventsSection, waypointModel});
-
+const mainPresenter = new MainPresenter({
+  tripMain: tripMainElement,
+  tripControlsFiltres: tripControlsFiltersElement,
+  tripEventsSection: tripEventsSection,
+  waypointModel,
+});
+const waypointPresenter = new WaypointPresenter({
+  waypointContainer: tripEventsSection,
+  waypointModel,
+});
 mainPresenter.init();
 waypointPresenter.init();
--- src/view/filters-view.js
+++ src/view/filters-view.js
@@ -1,44 +1,41 @@
-import { createElement } from '../render.js'
+import AbstractView from '../framework/view/abstract-view.js';
+import { FILTERS_OPTIONS } from '../const.js';
 
-function createTripFiltersElement() {
+function createTripFiltersElement(data) {
+  const isEmpty = data.length === 0;
+
   return ` <form class="trip-filters" action="#" method="get">
-  <div class="trip-filters__filter">
-    <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything" checked>
-    <label class="trip-filters__filter-label" for="filter-everything">Everything</label>
-  </div>
+  ${FILTERS_OPTIONS.map((elem) => {
+    let isDisabled = false;
+    if (elem === FILTERS_OPTIONS[1] || elem === FILTERS_OPTIONS[3]) {
+      if (isEmpty) {
+        isDisabled = true;
+      }
+    }
+    return `<div class="trip-filters__filter">
+      <input id="filter-${elem.toLocaleLowerCase()}" ${
+  isDisabled && 'disabled'
+} class="trip-filters__filter-input  visually-hidden"  type="radio" name="trip-filter" value="${elem.toLocaleLowerCase()} ${
+  elem.toLocaleLowerCase === 'past' ? 'checked' : ''
+}">
+      <label class="trip-filters__filter-label" for="filter-everything">${elem}</label>
+    </div>`;
+  }).join('')}
 
-  <div class="trip-filters__filter">
-    <input id="filter-future" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="future">
-    <label class="trip-filters__filter-label" for="filter-future">Future</label>
-  </div>
-
-  <div class="trip-filters__filter">
-    <input id="filter-present" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="present">
-    <label class="trip-filters__filter-label" for="filter-present">Present</label>
-  </div>
-
-  <div class="trip-filters__filter">
-    <input id="filter-past" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="past">
-    <label class="trip-filters__filter-label" for="filter-past">Past</label>
-  </div>
 
   <button class="visually-hidden" type="submit">Accept filter</button>
 </form>`;
 }
 
-export default class TripFiltersView {
-  getTemplate() {
-    return createTripFiltersElement();
+export default class TripFiltersView extends AbstractView {
+  #filtersData = '';
+
+  constructor(data) {
+    super();
+    this.#filtersData = data;
   }
 
-  getElement() {
-    if (!this.element) {
-      this.element = createElement(this.getTemplate());
-    }
-    return this.element;
-  }
-
-  removeElement() {
-    this.element = null;
+  get template() {
+    return createTripFiltersElement(this.#filtersData);
   }
 }
