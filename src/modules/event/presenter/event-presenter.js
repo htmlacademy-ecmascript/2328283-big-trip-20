9c98fd59a88d796a8779f7eef8b45a646ac87248
Alexander <nar.080@mail.ru>
2023-06-19 02:40:18 +0300

    6.9. Обновление века

--- src/modules/event/presenter/event-presenter.js
+++ src/modules/event/presenter/event-presenter.js
@@ -1,33 +1,32 @@
-import EventItemView from '../view/event-item-view.js';
-import EventFormItemView from '../view/event-form-item-view.js';
-
 import { render, replace, remove } from '../../../framework/render.js';
-import { isSameDate } from '../../../utils/date.js';
-import { EventMode, UserAction, UpdateType } from '../../../const.js';
+import { EventMode } from '../../../const.js';
+
+import EventItemView from '../view/event-item-view.js';
+import EventEditItemView from '../view/event-edit-item-view.js';
 
 export default class EventPresenter {
   #data = {};
 
   #eventsListContainer = null;
   #eventItem = null;
-  #eventFormItem = null;
+  #eventEditItem = null;
 
-  #onEventUserAction = null;
-  #onEventModeChange = null;
+  #rerenderEvent = null;
+  #changeEventMode = null;
 
   #mode = EventMode.DEFAULT;
 
-  constructor({ eventsListContainer, onEventUserAction, onEventModeChange }) {
+  constructor({ eventsListContainer, rerenderEvent, changeEventMode }) {
     this.#eventsListContainer = eventsListContainer;
-    this.#onEventModeChange = onEventModeChange;
-    this.#onEventUserAction = onEventUserAction;
+    this.#rerenderEvent = rerenderEvent;
+    this.#changeEventMode = changeEventMode;
   }
 
-  init({ destinations, offerTypes, event }) {
-    this.#data = { destinations, offerTypes, event };
+  init({ destinations, types, event }) {
+    this.#data = { destinations, types, event };
 
-    const prevEventItemComponent = this.#eventItem;
-    const prevEventEditItemComponent = this.#eventFormItem;
+    const prevEventItem = this.#eventItem;
+    const prevEventEditItem = this.#eventEditItem;
 
     this.#eventItem = new EventItemView({
       data: this.#data,
@@ -35,126 +34,73 @@ export default class EventPresenter {
       onFavoriteButtonClick: this.#onFavoriteButtonClick
     });
 
-    this.#eventFormItem = new EventFormItemView({
+    this.#eventEditItem = new EventEditItemView({
       data: this.#data,
       onRollupButtonClick: this.#onEditFormRollupButtonClick,
-      onFormSubmit: this.#onEditFormSubmit,
-      onButtonClick: this.#onDeleteButtonClick
+      onEditFormSubmit: this.#onEditFormSubmit
     });
 
-    if (prevEventItemComponent === null) {
+    if (prevEventItem === null) {
       render(this.#eventItem, this.#eventsListContainer.element);
       return;
     }
 
     if (this.#mode === EventMode.DEFAULT) {
-      replace(this.#eventItem, prevEventItemComponent);
+      replace(this.#eventItem, prevEventItem);
     }
 
     if (this.#mode === EventMode.EDITING) {
-      replace(this.#eventItem, prevEventEditItemComponent);
-      this.#mode = EventMode.DEFAULT;
+      replace(this.#eventEditItem, prevEventEditItem);
     }
 
-    remove(prevEventItemComponent);
-    remove(prevEventEditItemComponent);
-  }
-
-  setSaving() {
-    if (this.#mode === EventMode.EDITING) {
-      this.#eventFormItem.updateElement({
-        isDisabled: true,
-        isSaving: true
-      });
-    }
-  }
-
-  setDeleting() {
-    if (this.#mode === EventMode.EDITING) {
-      this.#eventFormItem.updateElement({
-        isDisabled: true,
-        isDeleting: true
-      });
-    }
-  }
-
-  setAborting() {
-    if (this.#mode === EventMode.DEFAULT) {
-      this.#eventItem.shake();
-      return;
-    }
-
-    const resetFormDisabling = () =>
-      this.#eventFormItem.updateElement({
-        isDisabled: false,
-        isSaving: false,
-        isDeleting: false
-      });
-
-    this.#eventFormItem.shake(resetFormDisabling);
+    remove(prevEventItem);
+    remove(prevEventEditItem);
   }
 
   destroy() {
     remove(this.#eventItem);
-    remove(this.#eventFormItem);
+    remove(this.#eventEditItem);
   }
 
-  closeEventEditForm() {
+  closeForm() {
     if (this.#mode !== EventMode.DEFAULT) {
-      this.#eventFormItem.reset();
+      this.#eventEditItem.reset();
       this.#replaceEditFormToEventItem();
     }
   }
 
-  #replaceEventItemToEditForm() {
-    replace(this.#eventFormItem, this.#eventItem);
-    this.#onEventModeChange();
-    this.#mode = EventMode.EDITING;
-    document.addEventListener('keydown', this.#onDocumentEscapeKeydown);
-  }
-
-  #replaceEditFormToEventItem() {
-    replace(this.#eventItem, this.#eventFormItem);
-    this.#mode = EventMode.DEFAULT;
-    document.removeEventListener('keydown', this.#onDocumentEscapeKeydown);
+  updateMode(mode) {
+    this.#mode = mode;
   }
 
   #onEventRollupButtonClick = () => this.#replaceEventItemToEditForm();
 
-  #onEditFormRollupButtonClick = () => this.closeEventEditForm();
+  #onFavoriteButtonClick = () => this.#rerenderEvent({ ...this.#data.event, isFavorite: !this.#data.event.isFavorite });
 
-  #onFavoriteButtonClick = () => this.#onEventUserAction(
-    UserAction.UPDATE_EVENT,
-    UpdateType.PATCH,
-    { ...this.#data.event, isFavorite: !this.#data.event.isFavorite }
-  );
+  #onEditFormRollupButtonClick = () => this.closeForm();
 
   #onEditFormSubmit = (updatedEvent) => {
-    const isPricesEqual = this.#data.event.basePrice !== updatedEvent.basePrice;
-    const isDatesFromEqual = !isSameDate(this.#data.event.dateFrom, updatedEvent.dateFrom);
-    const isDatesToEqual = !isSameDate(this.#data.event.dateTo, updatedEvent.dateTo);
-    const isMinorUpdate = isPricesEqual || isDatesFromEqual || isDatesToEqual;
-
-    this.#onEventUserAction(
-      UserAction.UPDATE_EVENT,
-      isMinorUpdate ? UpdateType.MINOR : UpdateType.PATCH,
-      updatedEvent
-    );
-  };
-
-  #onDeleteButtonClick = (updatedEvent) => {
-    this.#onEventUserAction(
-      UserAction.DELETE_EVENT,
-      UpdateType.MINOR,
-      updatedEvent
-    );
+    this.#replaceEditFormToEventItem();
+    this.#rerenderEvent(updatedEvent);
   };
 
   #onDocumentEscapeKeydown = (evt) => {
     if (evt.key === 'Escape') {
       evt.preventDefault();
-      this.closeEventEditForm();
+      this.closeForm();
       document.removeEventListener('keydown', this.#onDocumentEscapeKeydown);
     }
   };
+
+  #replaceEventItemToEditForm() {
+    replace(this.#eventEditItem, this.#eventItem);
+    this.#changeEventMode(this.#mode, this.#data.event.id);
+    document.addEventListener('keydown', this.#onDocumentEscapeKeydown);
+  }
+
+  #replaceEditFormToEventItem() {
+    replace(this.#eventItem, this.#eventEditItem);
+    this.#changeEventMode(this.#mode, this.#data.event.id);
+    document.removeEventListener('keydown', this.#onDocumentEscapeKeydown);
+  }
 }
